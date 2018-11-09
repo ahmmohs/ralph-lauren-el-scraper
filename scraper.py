b2bd13d7180242148a5233b2019e059066920615
aws <36905308+ahmmohs@users.noreply.github.com>
2018-11-09 01:19:01 -0500

    Create scraper.py

---
+++ scraper.py
@@ -0,0 +1,25 @@
+import requests
+import threading
+
+url = 'https://www.ralphlauren.com/p/p/'
+starting_pid = 480057
+threads = []
+
+print("--------------------------------------------")
+print("      RALPH LAUREN EARLY LINK SCRAPER       ")
+print("                by @ahmMohs                 ")
+print("--------------------------------------------")
+
+def checkUrl(x):
+    try:
+        r = requests.get(x)
+        if 'hidden' in r.url:
+            print(r.url)
+    except:
+        pass
+
+for x in range(starting_pid, 481100):
+    curl = url + str(x) + '.html'
+    t = threading.Thread(target=checkUrl, args=(curl,))
+    t.start()
+    threads.append(t)
